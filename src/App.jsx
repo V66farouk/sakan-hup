6a6f3751cb46a9cf2ee5663d7b2da1dc6c3c3883
faroukk <farouk@example.com>
2026-01-14 12:40:15 -0800

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import{ BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/signin';
+import Profile from './pages/profile';
+import SignUp from './pages/signUp';
+import About from './pages/about';
+     
 export default function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen bg-slate-100">
-      <h1 className="text-6xl font-extrabold text-blue-600 italic">
-        Tailwind is Working!
-      </h1>
-      <p className="mt-4 text-2xl text-red-500 bg-yellow-200 px-4 py-2 rounded-lg">
-        اختبار الألوان والخلفية
-      </p>
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+<Route path="/" element={<Home />} />
+<Route path="/Sign-In" element={<SignIn />} />
+<Route path="/Profile" element={<Profile />} />
+<Route path="/SignUp" element={<SignUp />} />
+<Route path="/About" element={<About />} />
+  </Routes>
+  </BrowserRouter>
+  
 }
 
 
